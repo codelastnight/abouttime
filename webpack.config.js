fe3c984a9646ef64c85909ad42ddf300b228e676
last night <fujizhang2525@gmail.com>
2021-02-06 21:11:44 -0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require( 'path' );
+
+module.exports = {
+
+    // bundling mode
+    mode: 'production',
+
+    // entry files
+    entry: './src/ts/index.ts',
+
+    // output bundles (location)
+    output: {
+        path: path.resolve( __dirname, 'dist' ),
+        filename: 'main.js',
+    },
+    plugins: [new HtmlWebpackPlugin()],
+
+    // file resolutions
+    resolve: {
+        extensions: [ '.ts', '.js' ],
+    },
+
+    // loaders
+    module: {
+        rules: [
+            {
+                test: /\.tsx?/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+              },
+        ]
+    }
+};
